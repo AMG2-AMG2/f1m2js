b90c31c1ed6203c21718e3ade3014077f7e6318d
Halil Dogan <33289@ma-web.nl>
2021-12-21 12:16:38 +0100

    commit 7

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Halil Dogan 
 
-[Live versie van de opdrachten](33289.hosts1.ma-cloud.nl)
+[Opdracht 1 - Background Color](33289.hosts1.ma-cloud.nl/web/les2-boodschappen)
--- les2-boodschappen/script.js
+++ les2-boodschappen/script.js
@@ -1,1 +1,21 @@
- console.log("Script is geladen!");
+console.log("Script is geladen!");
+
+const groentevak = document.getElementById('groente')
+const fruitvak = document.getElementById('fruit')
+
+function zetInFruitLa(fruit){
+    console.log(fruit)
+
+    const nieuwElemet = document.createElement('h2');
+    nieuwElemet.innerHTML = fruit;
+    nieuwElemet.className = 'rood';
+    fruitvak.appendChild(nieuwElemet);
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente)
+    const nieuwElemet = document.createElement('h2');
+    nieuwElemet.innerHTML = groente;
+    nieuwElemet.className = 'Groen';
+    groentevak.appendChild(nieuwElemet);
+}
